a5e5bf1c7dd5084b98bd62fde62f52434609c2c4
ilsenzanima <104711785+ilsenzanima@users.noreply.github.com>
2026-01-20 06:28:05 +0100

    feature: add service worker for PWA installation support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { AuthProvider } from "@/components/auth-provider";
 import { ConnectionManager } from "@/components/ConnectionManager";
 import { Toaster } from "@/components/ui/toaster";
+import { ServiceWorkerRegistration } from "@/components/ServiceWorkerRegistration";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -68,6 +69,7 @@ export default function RootLayout({
         >
           <AuthProvider>
             <ConnectionManager />
+            <ServiceWorkerRegistration />
             {children}
             <Toaster />
           </AuthProvider>
